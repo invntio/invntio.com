e978f44655ddd7800c9e22e993f510cf0bb5dc0c
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-04-13 20:07:24 -0400

    Change config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./public//*.html', './src//*.{astro,js,jsx,ts,tsx,vue}'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   presets: [],
   darkMode: 'class', // or 'media'
