bccbdb33ef56533b7a0eb9e44c926f9f3757e190
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2025-07-16 14:49:17 -0400

    feat: upgrade to new astro version 5.11.2 and add cloudflare compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    integrations: [tailwind()],
+    output: "static",
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true,
+        },
+    }),
+    site: "https://bohio.app",
+    vite: {
+        server: {
+            allowedHosts: [
+                "localweb.bohio.app",
+                "bohio.app",
+                "www.bohio.app",
+                "localhost",
+            ],
+        },
+    },
 });
